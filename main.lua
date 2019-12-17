3684708c76b1365d8acf26e34269d51f85d09351
jesseCat55 <jesse13884@gmail.com>
2019-12-17 11:42:20 +0000

    init

---
+++ main.lua
@@ -0,0 +1,165 @@
+
+local screenH = love.graphics.getHeight()
+local screenW = love.graphics.getWidth()
+local smallerCube = math.min(screenH, screenW)
+local maxTilesInScreen = 10
+local tileSize = math.floor(smallerCube/maxTilesInScreen)
+local xPos = (1.5 * tileSize) + tileSize
+local yPos = 1.5 * tileSize
+local fps = 50
+local timeFromPrevFrame = 0
+local pressedKey = ""
+local movementSpeed = 5
+local prevKeyPressed = ""
+
+function love.load()
+  love.graphics.setColor(0, 0, 0, 255)
+  love.graphics.setBackgroundColor(0, 0, 0)
+end
+
+function love.draw()
+  draw()
+  drawPlayer(xPos, yPos)
+end
+
+function draw()
+  local mode = 1
+  local theMode
+  -- 1, maxTilesInScreen - 2 -> create an offset of 1 tile size from (0,0)
+  for i =1, maxTilesInScreen - 2, 1 do
+    for j = 1, maxTilesInScreen - 2, 1 do
+      if mode == 1 then
+        love.graphics.setColor(140, 0, 255)
+      else
+        love.graphics.setColor(10, 255, 10)
+      end
+      mode = 1 - mode
+      love.graphics.rectangle("fill", j*tileSize, i*tileSize, tileSize, tileSize )
+    end
+    mode = 1 - mode
+  end
+end
+
+function drawPlayer(x, y)
+  love.graphics.setColor(0, 255, 255)
+  love.graphics.circle( "fill", x, y, tileSize/2 )
+end
+
+function love.keypressed(key, unicode)
+  if key == "up" or key == "down" or key == "left" or key == "right" then
+    if pressedKey == "" or prevKeyPressed == pressedKey then
+    else
+      prevKeyPressed = pressedKey
+    end
+    pressedKey = key
+  end
+end
+
+function love.update(dt)
+  timeFromPrevFrame = timeFromPrevFrame + dt
+  if timeFromPrevFrame > 1/fps then
+    timeFromPrevFrame = 0
+    updatePos()
+  end
+end
+
+function updatePos()
+print("updatePos")
+  if pressedKey == "up" and yPos % 1.5 * tileSize == 0 then
+    print("move up valid")
+    if yPos == 1.5 * tileSize then
+      print("died")
+    else
+      print("moved up")
+      yPos = yPos - movementSpeed
+      if yPos < 1.5 * tileSize then
+        yPos = 1.5 * tileSize
+      end
+    end
+  else
+    if pressedKey == "down" and yPos % 1.5 * tileSize == 0 then
+      print("move down valid")
+      if yPos == tileSize * 8.5 then
+        print("died")
+      else
+        print("moved down")
+        yPos = yPos + movementSpeed
+        if yPos > tileSize * 8.5 then
+          yPos = tileSize * 8.5
+        end
+      end
+    else
+      if pressedKey == "left" and xPos % 1.5 * tileSize == 0 then
+        print("move left valid")
+        if xPos == 1.5 * tileSize then
+          print("died")
+        else
+          print("moved left")
+          xPos = xPos - movementSpeed
+          if xPos < 1.5 * tileSize then
+            xPos = 1.5 * tileSize
+          end
+        end
+      else
+        if pressedKey == "right" and xPos % 1.5 * tileSize == 0 then
+          print("move right valid")
+          if xPos == tileSize * 8.5 then
+            print("died")
+          else
+            print("moved right")
+            xPos = xPos + movementSpeed
+            if xPos > tileSize * 8.5 then
+              xPos = tileSize * 8.5
+            end
+          end
+        else
+          print("moved as before")
+          moveAsBefore()
+        end
+      end
+    end
+  end
+end
+
+function moveAsBefore()
+if prevKeyPressed == "up" then
+    if yPos == 1.5 * tileSize then
+      print("died")
+    else
+      yPos = yPos - movementSpeed
+      if yPos < 1.5 * tileSize then
+        yPos = 1.5 * tileSize
+      end
+    end
+  end
+  if prevKeyPressed == "down" then
+    if yPos == tileSize * 8.5 then
+      print("died")
+    else
+      yPos = yPos + movementSpeed
+      if yPos > tileSize * 8.5 then
+        yPos = tileSize * 8.5
+      end
+    end
+  end
+  if prevKeyPressed == "left" then
+    if xPos == 1.5 * tileSize then
+      print("died")
+    else
+      xPos = xPos - movementSpeed
+      if xPos < 1.5 * tileSize then
+        xPos = 1.5 * tileSize
+      end
+    end
+  end
+  if prevKeyPressed == "right" then
+    if xPos == tileSize * 8.5 then
+      print("died")
+    else
+      xPos = xPos + movementSpeed
+      if xPos > tileSize * 8.5 then
+        xPos = tileSize * 8.5
+      end
+    end
+  end
+end
