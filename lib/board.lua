4ef12a477c2f74e89dfca9442091e952e1fafaa2
Carmel-Zaaroor <jesse.carmel-zaaroor@philips.com>
2019-12-17 14:37:08 +0200

    updated board module

--- lib/board.lua
+++ lib/board.lua
@@ -1,8 +1,33 @@
-local myBoard =  {}
+local board =  {}
 
-function myBoard.load()
+local screenH = love.graphics.getHeight()
+local screenW = love.graphics.getWidth()
+local smallerCube = math.min(screenH, screenW)
+local maxTilesInScreen = 10
+local tileSize = math.floor(smallerCube/maxTilesInScreen)
+
+function board.load()
     love.graphics.setColor(0, 0, 0, 255)
     love.graphics.setBackgroundColor(0, 0, 0)
 end
 
-return myBoard
+
+function board.draw()
+    local mode = 1
+    local theMode
+    -- 1, maxTilesInScreen - 2 -> create an offset of 1 tile size from (0,0)
+    for i =1, maxTilesInScreen - 2, 1 do
+      for j = 1, maxTilesInScreen - 2, 1 do
+        if mode == 1 then
+          love.graphics.setColor(140, 0, 255)
+        else
+          love.graphics.setColor(10, 255, 10)
+        end
+        mode = 1 - mode
+        love.graphics.rectangle("fill", j*tileSize, i*tileSize, tileSize, tileSize )
+      end
+      mode = 1 - mode
+    end
+  end
+
+return board
--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 
+
 local screenH = love.graphics.getHeight()
 local screenW = love.graphics.getWidth()
 local smallerCube = math.min(screenH, screenW)
@@ -11,33 +12,15 @@ local timeFromPrevFrame = 0
 local pressedKey = ""
 local movementSpeed = 5
 local prevKeyPressed = ""
-local board = require('myBoard')
+local board = require('board')
 
 function love.load()
   board.load()
 end
 
 function love.draw()
-  draw()
+  board.draw()
   drawPlayer(xPos, yPos)
-end
-
-function draw()
-  local mode = 1
-  local theMode
-  -- 1, maxTilesInScreen - 2 -> create an offset of 1 tile size from (0,0)
-  for i =1, maxTilesInScreen - 2, 1 do
-    for j = 1, maxTilesInScreen - 2, 1 do
-      if mode == 1 then
-        love.graphics.setColor(140, 0, 255)
-      else
-        love.graphics.setColor(10, 255, 10)
-      end
-      mode = 1 - mode
-      love.graphics.rectangle("fill", j*tileSize, i*tileSize, tileSize, tileSize )
-    end
-    mode = 1 - mode
-  end
 end
 
 function drawPlayer(x, y)
